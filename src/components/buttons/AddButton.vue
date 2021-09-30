93da8bb8ec3bb7ab36f9dfb4a27c59f9a9473c67
David <raymondinf23@gmail.com>
2021-09-30 00:15:39 -0400

    Create AddButton

---
+++ src/components/buttons/AddButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button>Add</button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    button {
+        background-color: #2F80ED;
+        border: none;
+        border-radius: 12px;
+        color: #fff;
+        cursor: pointer;
+        padding: 15px 25px;
+    }
+</style>
