6e4918ce62cfa1d866888e0556c7cf5876bd3b36
David <raymondinf23@gmail.com>
2021-09-30 00:14:00 -0400

    Create a reusable Icon component

---
+++ src/components/common/Icon.vue
@@ -0,0 +1,21 @@
+<template>
+    <img class="icon" :src="src">
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  width: 20px;
+  cursor: pointer;
+}
+</style>
