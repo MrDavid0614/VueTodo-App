7b80a3adb193cccdcc84c41813ddd6f2789242e1
David <raymondinf23@gmail.com>
2021-09-30 00:15:55 -0400

    Create DeleteButton

---
+++ src/components/buttons/DeleteButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <div @click="$emit('button-clicked')" class="delete-btn-container">
+      <Icon :src="deleteIcon" />
+      <button class="delete-btn">Delete all</button>
+  </div>
+</template>
+
+<script>
+import Icon from '../common/Icon.vue'
+import deleteIcon from '@/assets/icons/delete.svg'
+
+export default {
+  data () {
+    return {
+      deleteIcon
+    }
+  },
+  components: {
+    Icon
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.delete-btn-container {
+    grid-column-start: 3;
+    display: flex;
+    background-color: #EB5757;
+    padding: 10px;
+    cursor: pointer;
+    border-radius: 4px;
+
+    .delete-btn {
+        border: none;
+        background-color: inherit;
+        color: #fff;
+        cursor: inherit;
+    }
+}
+</style>
